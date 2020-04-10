41f148a0f0613622eb5e0231ce1c6d3c3236e709
Scott Stoll <scottstoll2017@gmail.com>
2020-04-10 16:17:51 -0400

    Icons examples changed so they don't match the exercise and solution.

--- lib/widgets/examples/e_icon_examples.dart
+++ lib/widgets/examples/e_icon_examples.dart
@@ -20,59 +20,100 @@ class IconExamples extends StatelessWidget {
           children: <Widget>[
             _xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx,
             ExampleIconCustomWidget(
-              color: const Color(0xFF777777),
-              //----------------------------------------------
-              //----------------------------------------------
+              color: Colors.green,
               icon: Icon(
-                Icons.g_translate,
-                semanticLabel: 'Google Translate',
-                size: 80,
+                Icons.traffic,
+                semanticLabel: 'Traffic Light',
+                size: 132,
                 color: Colors.white54,
               ),
-              //----------------------------------------------
-              //----------------------------------------------
+            ),
+            _xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx,
+            Container(
+              height: 200,
+              width: 200,
+              margin: const EdgeInsets.only(top: 30),
+              padding: const EdgeInsets.all(30),
+              decoration: BoxDecoration(
+                border: Border.all(
+                  style: BorderStyle.solid,
+                  color: const Color(0xFFBBBBBB),
+                  width: 3,
+                ),
+                borderRadius: BorderRadius.circular(100),
+                boxShadow: [
+                  const BoxShadow(
+                    color: Colors.black87,
+                    blurRadius: 12,
+                    offset: Offset(4, 6),
+                  ),
+                ],
+                gradient: const SweepGradient(
+                  colors: [
+                    Color(0xFFFB7EE4),
+                    Color(0xFFB7459C),
+                    Color(0xFF01D9FE),
+                    Color(0xFF0185D0),
+                    Color(0xFFFB7EE4),
+                  ],
+                  stops: [0.0, 0.25, 0.5, 0.75, 1],
+                ),
+              ),
+              child: Icon(
+                Icons.audiotrack,
+                size: 132,
+                color: Colors.white24,
+              ),
+            ),
+            _xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx,
+            Container(
+              height: 200,
+              width: 200,
+              margin: const EdgeInsets.only(top: 30),
+              padding: const EdgeInsets.all(30),
+              decoration: BoxDecoration(
+                border: Border.all(
+                  style: BorderStyle.solid,
+                  color: const Color(0xFFBBBBBB),
+                  width: 3,
+                ),
+                borderRadius: BorderRadius.circular(100),
+                boxShadow: [
+                  const BoxShadow(
+                    color: Colors.black87,
+                    blurRadius: 12,
+                    offset: Offset(4, 6),
+                  ),
+                ],
+                gradient: const RadialGradient(
+                  colors: [
+                    Color(0xFF01D9FE),
+                    Color(0xFF0185D0),
+                    Color(0xFFFB7EE4),
+                    Color(0xFFB7459C),
+                  ],
+                  stops: [
+                    0.3,
+                    0.5,
+                    0.8,
+                    1,
+                  ],
+                  center: Alignment(0.0, 1.3),
+                  focal: Alignment(0.0, 0.9),
+                  focalRadius: 2.0,
+                ),
+              ),
+              child: Icon(Icons.audiotrack, size: 132, color: Colors.black38),
             ),
             _xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx,
             ExampleIconCustomWidget(
-              color: Colors.purple,
-              //----------------------------------------------
-              //----------------------------------------------
+              color: Colors.teal,
               icon: Icon(
-                Icons.assistant,
+                Icons.tv,
                 semanticLabel: 'Google Assistant',
                 size: 120,
-                color: Colors.white54,
+                color: Colors.deepOrangeAccent,
               ),
-              //----------------------------------------------
-              //----------------------------------------------
-            ),
-            _xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx,
-            ExampleIconCustomWidget(
-              color: Colors.white,
-              //----------------------------------------------
-              //----------------------------------------------
-              icon: Icon(
-                Icons.alarm,
-                semanticLabel: 'Red Alarm',
-                size: 80,
-                color: Colors.red,
-              ),
-              //----------------------------------------------
-              //----------------------------------------------
-            ),
-            _xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx,
-            ExampleIconCustomWidget(
-              color: Colors.red,
-              //----------------------------------------------
-              //----------------------------------------------
-              icon: Icon(
-                Icons.power_settings_new,
-                semanticLabel: 'Power Button',
-                size: 80,
-                color: Colors.white,
-              ),
-              //----------------------------------------------
-              //----------------------------------------------
             ),
             _xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx,
           ],
