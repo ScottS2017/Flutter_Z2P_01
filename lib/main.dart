8f3a8416264e3aa3deb61b3733e3a0eff74d6d04
Scott Stoll <scottstoll2017@gmail.com>
2020-04-19 14:00:38 -0400

    Updated App Launcher names in Android and iOS

--- lib/main.dart
+++ lib/main.dart
@@ -9,6 +9,7 @@ class WidgetsYouCanSee extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Z2P Widgets You Can See',
       theme: ThemeData(
         primarySwatch: Colors.blue,
